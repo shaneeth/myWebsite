6798579a9ef115f400e9f0e2ab151d424e24b332
unknown <shaneeth@live.in>
2015-01-02 00:28:48 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+    <title>Shaneeth</title>
+    <body>
+        <h1>COMING SOON ...</h1>
+    </body>
+</html>
---
+++ pages/blog.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaneeth-laptop
+ * Date: 12/26/2014
+ * Time: 1:28 AM
+ */
+
+echo "Blog test";
