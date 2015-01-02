34b43711e5ab97c337852535eb270d3c6232bf2a
Shaneeth <shaneeth@users.noreply.github.com>
2015-01-03 04:51:02 +0530

    Added index

--- index.php
+++ index.php
@@ -1,6 +1,78 @@
+<!DOCTYPE html>
 <html>
-    <title>Shaneeth</title>
+    <head>
+        <!-- Start meta tags -->
+        <meta charset="utf-8">
+        <meta name="description" content="Shaneeth's Chooky Dhooky Ehooky No Meaning Just Madness.\n
+            My personal website.\n Showcase the works done by me & connect me links.">
+        <meta name="keywords" content="shaneeth, website, personal, blog, creative, videos, links,
+            connect, profile, about, me, about me, works, my, my works, chooky, dhooky, ehooky, no, meaning, just, madness, etc">
+        <!-- End meta tags -->
+
+        <title>Shaneeth - Personal Website</title>
+        <link rel="shortcut icon" href="images/myIcon.ico">
+
+        <!-- Start style tags -->
+        <link rel="stylesheet" type="text/css" href="styles/external/magnific-popup.css">
+        <link rel="stylesheet/less" type="text/css" href="styles/style.less">
+        <!-- End style tags-->
+
+        <!-- Start script tags -->
+        <script type="text/javascript" src="scripts/external/less.min.js"></script>
+        <script type="text/javascript" src="scripts/external/jQuery.min.js"></script>
+        <script type="text/javascript" src="scripts/external/magnific.popup.min.js"></script>
+        <script type="text/javascript" src="scripts/script.js"></script>
+        <!-- End scripts tags -->
+    </head>
     <body>
-        <h1>COMING SOON ....</h1>
+        <!-- Start header -->
+        <header class="top">
+            <!-- Start head face -->
+            <div class="head-face">
+                <div class="face-bg">
+                    <div class="eyes-wrap">
+                        <div class="left-eye"></div>
+                        <div class="right-eye"></div>
+                    </div>
+                </div>
+                <div class="blog-face"></div>
+            </div>
+            <!-- End head face -->
+        </header>
+        <!-- End header -->
+
+        <!-- Start content wrapper -->
+        <div class="content">
+            <!-- Start landing -->
+            <section id="landing"></section>
+            <!-- End landing -->
+
+            <!-- Start about me -->
+            <section id="about-me"></section>
+            <!-- End about me -->
+
+            <!-- Start my works -->
+            <section id="my-works">
+                <a class="work-link" href="https://www.youtube.com/watch?v=uit1amuX1Oo">Desto</a>
+                <a class="work-link" href="https://www.youtube.com/watch?v=n9leuyxaK8w">CAD</a>
+            </section>
+            <!-- End my works -->
+
+            <!-- Start connect me -->
+            <section id="connect-me"></section>
+            <!-- ENd connect me -->
+        </div>
+        <!-- End content wrapper -->
+
+        <!-- Start menu -->
+        <nav class="menu">
+            <ul>
+                <li>Home</li>
+                <li>About me</li>
+                <li>Works</li>
+                <li>Connect</li>
+            </ul>
+        </nav>
+        <!-- End menu -->
     </body>
 </html>
