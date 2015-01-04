8b844c7cb54b5fd7249c6a0ef667b476d4408d6b
Shaneeth <shaneeth@users.noreply.github.com>
2015-01-04 06:09:08 +0530

    Complete landing page, Adding about me.

--- index.php
+++ index.php
@@ -20,6 +20,8 @@
         <!-- Start script tags -->
         <script type="text/javascript" src="scripts/external/less.min.js"></script>
         <script type="text/javascript" src="scripts/external/jQuery.min.js"></script>
+        <script type="text/javascript" src="scripts/external/jquery-ui.js"></script>
+        <script type="text/javascript" src="scripts/external/flux.min.js"></script>
         <script type="text/javascript" src="scripts/external/magnific.popup.min.js"></script>
         <script type="text/javascript" src="scripts/script.js"></script>
         <!-- End scripts tags -->
@@ -35,7 +37,9 @@
                         <div class="right-eye"></div>
                     </div>
                 </div>
-                <div class="blog-face"></div>
+                <div class="blog-face">
+                    <a href="">Blog</a>
+                </div>
             </div>
             <!-- End head face -->
         </header>
@@ -44,11 +48,47 @@
         <!-- Start content wrapper -->
         <div class="content">
             <!-- Start landing -->
-            <section id="landing"></section>
+            <section id="landing">
+                <div class="clouds-logo">
+                    <div class="cloud1 cloud-bg-1"></div>
+                    <div class="cloud2 cloud-bg-1"></div>
+                    <div class="cloud3 cloud-bg-2"></div>
+                    <div class="cloud4 cloud-bg-2"></div>
+                    <div class="logo">Shaneeth's</div>
+                </div>
+                <div class="birds-fly">
+                    <div class="bird1"></div>
+                    <div class="bird2"></div>
+                    <div class="bird3"></div>
+                </div>
+                <div class="my-ship"></div>
+                <div class="chooky-ship"></div>
+                <div class="ehooky-ship"></div>
+                <div class="dhooky-ship"></div>
+                <div class="hill-back-bg"></div>
+                <div class="hill-front-left"></div>
+                <div class="hill-front-right"></div>
+                <div class="sea-bg"></div>
+            </section>
             <!-- End landing -->
 
             <!-- Start about me -->
-            <section id="about-me"></section>
+            <section id="about-me">
+                <div id="my-photos-slider">
+                    <?php
+                        function render_photos() {
+                            $image_count = 7;
+                            for($i = 1; $i <= $image_count; $i++) {
+                                echo "<img src=\"images/myPhotos/$i.png\">";
+                            }
+                        }
+                        render_photos();
+                    ?>
+                </div>
+                <p class="description">
+                    I'm very smart and curious about everything. However, I don't like too much noise and chaos and prefer to be alone in a quiet place. Usually I sit upstairs in a small room/ a chair and a TV and just relax and think after school as a sort of "unwinding" time. I'm funny, and I make cracks and jokes about everything, but sometimes I go too far, and I don't really notice it. I can't read people's cues and unspoken rules and whatnot, so "social norms" and "unspoken rules" and things like that are beyond me. I say what I feel about myself, a person, an event, anything and I say it out loud and proud. I don't care if people get offended by my blunt truth, because I believe that people are too sheltered from reality these days. I am constantly criticizing myself, and since I love to write and draw, I'm always editing things, always trying to achieve perfection in everything I do. This makes me a perfectionist and a procrastinator, and if I am assigned a project that says "do anything you want" I'm frozen. I need it to be narrowed down, I can't just go off and do ANYTHING. Give me some limitations, please.
+                </p>
+            </section>
             <!-- End about me -->
 
             <!-- Start my works -->
@@ -68,57 +108,45 @@
         <nav class="menu">
             <ul>
                 <li>
-                    <div class="emenu active">
-                        <div class="icon">
-                        </div>
-                        <a href="#landing">
-                            <div class="lite">
-													                Home
-                            </div>
-                            <div class="dark">
-                                Home
-                            </div>
-                        </a>
-                    </div>
-													</li>
-                <li>
                     <div class="emenu">
-                        <div class="icon">
-                        </div>
-                        <a href="#aboutme">
-                            <div class="lite">
-													                About me
-                            </div>
+                        <div class="icon icon-home"></div>
+                        <a class="menu-link" href="#landing">
+                            <div class="lite">Home</div>
                             <div class="dark">
-                                About me
+                                <div class="dark-in">Home</div>
                             </div>
                         </a>
                     </div>
                 </li>
                 <li>
                     <div class="emenu">
-                        <div class="icon">
-                        </div>
-                        <a href="#works">
-                            <div class="lite">
-													                Works
-                            </div>
+                        <div class="icon icon-about-me"></div>
+                        <a class="menu-link" href="#about-me">
+                            <div class="lite">About me</div>
                             <div class="dark">
-                                Works
+                                <div class="dark-in">About me</div>
                             </div>
                         </a>
                     </div>
                 </li>
                 <li>
                     <div class="emenu">
-                        <div class="icon">
-                        </div>
-                        <a href="#connect">
-                            <div class="lite">
-													                Connect
-                            </div>
+                        <div class="icon icon-works"></div>
+                        <a class="menu-link" href="#my-works">
+                            <div class="lite">Works</div>
                             <div class="dark">
-                                Connect
+                                <div class="dark-in">Works</div>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="emenu">
+                        <div class="icon icon-connect"></div>
+                        <a class="menu-link" href="#connect-me">
+                            <div class="lite">Connect</div>
+                            <div class="dark">
+                                <div class="dark-in">Connect</div>
                             </div>
                         </a>
                     </div>
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,76 @@
 $(document).ready(function() {
+
+    var htmlBody = $('html, body');
+    htmlBody.animate({
+        scrollTop: 0
+    }, 1000, 'easeOutQuad');
+
+    /*
+     *  Scripts related to menus
+     */
+    var navMenus = $('nav.menu .emenu'),
+        navMenuSelected = $(navMenus[0]);
+    navMenuSelected.addClass('active');
+    navMenus.on('click', function() {
+        navMenuSelected.removeClass('active');
+        navMenuSelected = $(this);
+        navMenuSelected.addClass('active');
+    });
+    function setActiveMenu(currentElem) {
+        navMenuSelected.removeClass('active');
+        navMenuSelected = $(currentElem);
+        navMenuSelected.addClass('active');
+    }
+    function moveToSection(sel) {
+        htmlBody.stop(true);
+        htmlBody.animate({
+            scrollTop: $(sel).offset().top
+        }, 1000, 'easeOutQuad');
+    }
+    $('a.menu-link').on('click', function(event) {
+        moveToSection($(this).attr('href'));
+        event.preventDefault();
+    });
+
+    /*
+     *  Scripts related to content
+     */
+    var aboutElem = $("#about-me"),
+        worksElem = $("#my-works"),
+        connectElem = $("#connect-me"),
+        tempActiveMenu = '';
+    /*
+     *  Scripts related to scroll
+     */
+    function selectMenuOnScroll() {
+        tempActiveMenu = navMenus[0];
+        if($(this).scrollTop()+200 > aboutElem.offset().top) {
+            tempActiveMenu = navMenus[1];
+        }
+        if($(this).scrollTop()+200 > worksElem.offset().top) {
+            tempActiveMenu = navMenus[2];
+        }
+        if($(this).scrollTop()+200 > connectElem.offset().top) {
+            tempActiveMenu = navMenus[3];
+        }
+        setActiveMenu(tempActiveMenu);
+    }
+    $(window).on('scroll', function() {
+        selectMenuOnScroll();
+    });
+
+    /*
+     *  Scripts related to Photos slider
+     */
+    var fluxSlider = new flux.slider("#my-photos-slider", {
+        autoplay: true,
+        pagination: false,
+        delay: 5000
+    });
+
+    /*
+     *  Scripts related to Magnific popup
+     */
     $('a.work-link').magnificPopup({
         disableOn: 700,
         type: 'iframe',
@@ -7,4 +79,6 @@ $(document).ready(function() {
         preloader: false,
         fixedContentPos: false
     });
+
+
 });
